98d88c2bba030c35ab7f90672a2bfb2a0c9c53f3
Ed Summers <ehs@pobox.com>
2017-01-14 14:22:28 -0500

    upping version for release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ deps = [htmldiff]
 
 setup(
     name="diffengine",
-    version="0.0.20",
+    version="0.0.21",
     author="Ed Summers",
     author_email="ehs@pobox.com",
     packages=find_packages(exclude=['test_diffengine']),
