43f288d553ad8dd6015f5a6a4811e681a152469d
Ed Summers <ehs@pobox.com>
2017-03-11 14:07:07 +0000

    back to old template

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ deps = [htmldiff]
 
 setup(
     name="diffengine",
-    version="0.0.33",
+    version="0.0.34",
     author="Ed Summers",
     author_email="ehs@pobox.com",
     packages=find_packages(exclude=['test_diffengine']),
