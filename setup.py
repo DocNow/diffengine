b6b38f21971bbc1334a54808f142802d0e3fcdb8
Ed Summers <ehs@pobox.com>
2019-11-19 15:36:56 -0500

    new version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ htmldiff = "https://github.com/edsu/htmldiff/tarball/master#egg=htmldiff-0.2"
 reqs.remove(htmldiff)
 reqs.append("htmldiff==0.2")
 deps = [htmldiff]
-version = "0.1.2"
+version = "0.2.0"
 
 if __name__ == "__main__":
     setup(
