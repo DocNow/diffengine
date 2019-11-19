2b6bdc59e873fc6a601e01982fdc539888b55a20
Ed Summers <ehs@pobox.com>
2019-11-19 15:46:35 -0500

    send markdown with twine

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import sys
 if sys.version_info < (3,0):
     sys.exit('Sorry, diffengine runs on Python 3')
 
-
 from setuptools import setup, find_packages
 
 reqs = open("requirements.txt").read().split()
+
+with open("README.md") as f:
+    long_description = f.read()
 
 # hack until htmldiff is updated to work with python3 on pypi
 htmldiff = "https://github.com/edsu/htmldiff/tarball/master#egg=htmldiff-0.2"
@@ -21,7 +23,9 @@ if __name__ == "__main__":
         author="Ed Summers",
         author_email="ehs@pobox.com",
         packages=find_packages(exclude=['test_diffengine']),
-        description="Tweet changes to stories in RSS feeds",
+        description="Monitor changes to webpages in RSS feeds",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         install_requires=reqs,
         dependency_links=deps,
         setup_data={'diffengine': ['diffengine/diff.html']},
